49a503cfcb60dcdd286adaecda6359d1b066f3e3
coneasdev <coneastdev@proton.me>
2026-03-17 13:11:30 +0000

    update tests

---
+++ tests/test_apis.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import unittest
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from apis.apis import getGeoLocationFromPlaceName, getWeatherDataFromHeadings
+
+class TestGetGeoLocationFromPlaceName(unittest.TestCase):
+
+    def test_checkLocationOfLondon(self):
+        self.assertEqual(getGeoLocationFromPlaceName("London"), [51.50853, -0.12574])
+
+    def test_checkLocationOfMiddlesbroughCollege(self):
+        self.assertEqual(getGeoLocationFromPlaceName("middlesbrough"), [54.57623, -1.23483])
+
+class TestGetWeatherDataFromHeadings(unittest.TestCase):
+
+    def test_checkResponseOfLondon(self):
+        forecast = getWeatherDataFromHeadings(51.50853, -0.12574)
+        self.assertEqual(forecast["timezone"], "GMT")
+
+if __name__ == "__main__":
+    unittest.main()
