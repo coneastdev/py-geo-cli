fcfbf6882eee7954eb960d5356c3de74f919e368
coneasdev <coneastdev@proton.me>
2026-03-17 13:22:28 +0000

    add berlin to tests

--- tests/test_apis.py
+++ tests/test_apis.py
@@ -11,13 +11,17 @@ class TestGetGeoLocationFromPlaceName(unittest.TestCase):
     def test_checkLocationOfLondon(self):
         self.assertEqual(getGeoLocationFromPlaceName("London"), [51.50853, -0.12574])
 
-    def test_checkLocationOfMiddlesbroughCollege(self):
+    def test_checkLocationOfMiddlesbrough(self):
         self.assertEqual(getGeoLocationFromPlaceName("middlesbrough"), [54.57623, -1.23483])
 
 class TestGetWeatherDataFromHeadings(unittest.TestCase):
 
     def test_checkResponseOfLondon(self):
         forecast = getWeatherDataFromHeadings(51.50853, -0.12574)
+        self.assertTrue(forecast["timezone"] in ["GMT", "BST"])
+    
+    def test_checkResponseOfBerlin(self):
+        forecast = getWeatherDataFromHeadings(52.52437, 13.41053)
         self.assertEqual(forecast["timezone"], "GMT")
 
 if __name__ == "__main__":
